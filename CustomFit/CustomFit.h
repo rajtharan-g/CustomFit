e092fca8cbb1d63b941c4e1d55410497fd598f23
Rajtharan G <rajtharan@infivention.com>
2019-10-03 15:13:02 +0530

    Initial project setup

---
+++ CustomFit/CustomFit.h
@@ -0,0 +1,19 @@
+//
+//  CustomFit.h
+//  CustomFit
+//
+//  Created by Rajtharan G on 03/10/19.
+//  Copyright © 2019 Rajtharan G. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CustomFit.
+FOUNDATION_EXPORT double CustomFitVersionNumber;
+
+//! Project version string for CustomFit.
+FOUNDATION_EXPORT const unsigned char CustomFitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomFit/PublicHeader.h>
+
+
